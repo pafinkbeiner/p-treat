2791f407cb8b47e5046ca790e31a60bf5fdd698b
pafinkbeiner <paul-d-f@web.de>
2020-12-21 14:38:17 +0100

    sada

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Navbar.css";
+
+export interface Props {}
+
+export interface State {
+    actionButton: boolean;
+}
  
-class Navbar extends React.Component {
+class Navbar extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.state = { actionButton: false };
+    }
+
+    toggleActionButton = () => {
+        if(this.state.actionButton){
+            this.setState({ actionButton: false });
+        }else{
+            this.setState({ actionButton: true });
+        }
+
+    }
 
     render() { 
         return (  
@@ -11,7 +32,7 @@ class Navbar extends React.Component {
                         <div className="col"><Link to="/" className="brand-logo">pTreat</Link></div>
                         {/* Enabled on Mobile */}
                         <ul className="right hide-on-large-only">
-                        <li><a href="badges.html"><i className="material-icons">person</i></a></li>
+                            <li><a href="badges.html"><i className="material-icons">person</i></a></li>
                             <li><a href="mobile.html"><i className="material-icons">more_vert</i></a></li>
                         </ul>
                         {/* Enabled on Desktop */}
@@ -19,6 +40,45 @@ class Navbar extends React.Component {
                             <li><a href="badges.html"><i className="material-icons">person</i></a></li>
                             <li><a href="mobile.html"><i className="material-icons">more_vert</i></a></li>
                         </ul>
+
+                        <div className="fixed-action-btn">
+                        <button onClick={this.toggleActionButton} className="btn-floating btn-large orange">
+                            <i className="large material-icons">more_vert</i>
+                        </button>
+                    
+                        {
+
+                            !this.state.actionButton &&
+                            <div className="bottom-row">
+                            
+                                <div className="row">
+                                    <div className="col s12">                        
+                                        <button onClick={this.toggleActionButton} className="btn-floating btn-large red">
+                                            <i className="large material-icons">more_vert</i>
+                                        </button>
+                                    </div>
+                                    <div className="col s12">  
+                                        <button onClick={this.toggleActionButton} className="btn-floating btn-large yellow">
+                                            <i className="large material-icons">more_vert</i>
+                                        </button>
+                                    </div>
+                                    <div className="col s12">  
+                                        <button onClick={this.toggleActionButton} className="btn-floating btn-large green">
+                                            <i className="large material-icons">more_vert</i>
+                                        </button>
+                                    </div>
+                                    <div className="col s12">  
+                                        <button onClick={this.toggleActionButton} className="btn-floating btn-large blue">
+                                            <i className="large material-icons">more_vert</i>
+                                        </button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        }
+
+                        </div>
+
                     </div>
             
                 </div>
