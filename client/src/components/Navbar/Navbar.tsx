c1b1136cd997dcdcf5da79239c2b0c9057fae249
pafinkbeiner <paul-d-f@web.de>
2020-12-21 13:29:40 +0100

    Navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -11,13 +11,12 @@ class Navbar extends React.Component {
                         <div className="col"><Link to="/" className="brand-logo">pTreat</Link></div>
                         {/* Enabled on Mobile */}
                         <ul className="right hide-on-large-only">
+                        <li><a href="badges.html"><i className="material-icons">person</i></a></li>
                             <li><a href="mobile.html"><i className="material-icons">more_vert</i></a></li>
                         </ul>
                         {/* Enabled on Desktop */}
                         <ul className="right hide-on-med-and-down">
-                            <li><a href="sass.html"><i className="material-icons">home</i></a></li>
-                            <li><a href="badges.html"><i className="material-icons">view_module</i></a></li>
-                            <li><a href="collapsible.html"><i className="material-icons">wb_incandescent</i></a></li>
+                            <li><a href="badges.html"><i className="material-icons">person</i></a></li>
                             <li><a href="mobile.html"><i className="material-icons">more_vert</i></a></li>
                         </ul>
                     </div>
