b81fd3b059e786a7a6ba1e013d76b606fd604275
pafinkbeiner <paul-d-f@web.de>
2020-12-24 15:21:55 +0100

    removed comment

--- client/src/helper/jwtHelper.ts
+++ client/src/helper/jwtHelper.ts
@@ -5,13 +5,7 @@ export const refreshJWT = () => {
     .get(`${process.env.REACT_APP_API_URL}/refreshJWT`,  {headers: { Authorization: `Bearer ${localStorage.getItem("key")}` }})
     .then((auth) => {
 
-        console.log("Aktualisierter JWT: ", auth.data.key)
         localStorage.setItem("key", auth.data.key);
-
-    })
-    .catch((err) => {
-
-        console.log("Fehler beim Aktualisieren des JWT: ", localStorage.getItem("key"))
 
     });
 }
